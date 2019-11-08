aa3b7f0af202a963e8d2f9bbe42df280136f757c
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-11-08 11:18:15 +0100

    * fix

--- linux-builds/generic/install-boost.sh
+++ linux-builds/generic/install-boost.sh
@@ -6,7 +6,12 @@ source ./commons.sh
 
 download_targz_if_not_exist $BOOST_SOURCES boost_1_67_0
 
+# Boost does not listen to CC and CXX
+# Toolset must be specified manually
+TOOLSET=$($CC -dumpversion)
+TOOLSET=${TOOLSET%.*}
+
 pushd boost_1_67_0
 ./bootstrap.sh --prefix=$INSTALL_PREFIX
-./b2 --with-filesystem --with-program_options --with-log --with-regex --with-iostreams install
+./b2 --toolset=$TOOLSET --with-filesystem --with-program_options --with-log --with-regex --with-iostreams install
 popd
