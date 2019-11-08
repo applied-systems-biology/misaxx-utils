46fbac3e9113a87fd5ac1a3cdba6c060dbcd6a23
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-11-08 14:45:37 +0100

    * more sane number of building threads

--- linux-builds/generic/commons.sh
+++ linux-builds/generic/commons.sh
@@ -3,7 +3,7 @@
 INSTALL_PREFIX=$PWD/usr
 
 # Number of threads for make
-NUM_THREADS=30
+NUM_THREADS=4
 
 # Enable or disable OpenCV threading functionality (e.g. IPP, OpenCV), ON or OFF
 OPENCV_ENABLE_THREADING=ON
